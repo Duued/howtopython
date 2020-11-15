2100880c38a26087bd47757e673f88c9ca8cb30b
Duued <66633587+Duued@users.noreply.github.com>
2020-11-15 15:00:00 -0500

    Create basics.py
    more work needed but i gtg so yeah

---
+++ basics.py
@@ -0,0 +1,8 @@
+Hello, These are the basics of python to get your discord bot effective! 
+So first, we will introduce you to some very nice tools that can really help you with your issues!
+Discord.py docs: https://discordpy.readthedocs.io/en/latest
+Python docs: https://docs.python.org/3
+
+Basics:
+
+The character `#` allows you to comment, this is escaped in `""` strings
